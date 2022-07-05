02ebef6911228d1b07421a89df6166dcc8bfe7c2
James Mwendwa <mwendwajames32@gmail.com>
2022-07-05 22:44:50 +0300

    API intergration using axios

--- src/Form.js
+++ src/Form.js
@@ -1,18 +1,39 @@
 import { Button } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 // import Error from './Error';
 import './Form.css';
 
+import axios from 'axios';
+
+ const url = 'https://api.shrtco.de/v2';
+
 function Form() {
+
+  const [link, setLink] = useState([]);
+  const [error, setError] = useState(true);
+
+   const handleClick = (e) => {
+     e.preventDefault;
+     setLink()
+   };
+
+   useEffect(() => {
+
+   },[])
+
   return (
-    <div className='formPage'>
-        <form>
-            <input type="text" placeholder='Shorten a link here...' />
-        </form>
-        <Button>Shorten It!</Button>
-        
+    <div className="formPage">
+      <form>
+        <input
+          value={link}
+          onChange={(e) => e.target.value}
+          type="text"
+          placeholder="Shorten a link here..."
+        />
+      </form>
+      <Button onClick={handleClick}>Shorten It!</Button>
     </div>
-  )
+  );
 }
 
 export default Form;
