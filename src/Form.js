da1e00ba01e8948b8baea33a3059ef028627acc8
James Mwendwa <mwendwajames32@gmail.com>
2022-07-11 12:51:26 +0300

    api

--- src/Form.js
+++ src/Form.js
@@ -2,36 +2,42 @@ import { Button } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 // import Error from './Error';
 import './Form.css';
-
 import axios from 'axios';
 
- const url = 'https://api.shrtco.de/v2';
+function Form({inputValue}) {
 
-function Form() {
+  const [link, setLink] = useState('');
+  const [error, setError] = useState(false);
 
-  const [link, setLink] = useState([]);
-  const [error, setError] = useState(true);
 
-   const handleClick = (e) => {
-     e.preventDefault;
-     setLink()
+   const handleSubmit = (e) => {
+     e.preventDefault();
+     inputValue()
+
    };
 
    useEffect(() => {
+    fetch(`https://api.shrtco.de/v2/shorten?url=${inputValue}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setLink(data)
+    })
+    .catch((error) => console.log(error.message))
 
-   },[])
+   },[inputValue]);
 
   return (
     <div className="formPage">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           value={link}
-          onChange={(e) => e.target.value}
+          onChange={(e) => setLink(e.target.value)}
           type="text"
           placeholder="Shorten a link here..."
         />
+        <Button>Shorten It!</Button>
       </form>
-      <Button onClick={handleClick}>Shorten It!</Button>
     </div>
   );
 }
